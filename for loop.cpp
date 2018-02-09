34648cdd990a3123879dbfa0567df23941bb0b1d
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-02-09 08:30:02 +0300

    first commit

---
+++ for loop.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i =3; i<=5; i++)
+    {cout<< "hello buddy \n";
+    }
+
+    return 0;
+}
